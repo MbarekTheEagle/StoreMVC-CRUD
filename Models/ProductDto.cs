302e735d1b8242ce016a7f2838d206b207018a76
MbarekTheEagle <75943971+MbarekTheEagle@users.noreply.github.com>
2024-03-25 23:06:16 +0000

    Add files via upload

---
+++ Models/ProductDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreMVC.Models
+{
+    public class ProductDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        [Required]
+        public string Price { get; set; } = "";
+
+        [Required]
+        public string Description { get; set; } = "";
+
+        public IFormFile? ImageFile { get; set; } 
+
+    }
+}
---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreMVC.Models;
+
+namespace StoreMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //this code defines a class that represents the application's database context,
+        //allowing it to interact with the underlying database using Entity Framework Core.
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //Property that add a table in model for us it is called Product to the database
+        public DbSet<Product> Products { get; set; }
+    }
+}
